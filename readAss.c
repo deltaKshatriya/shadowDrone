b7aae4b94b8e0bf603323cec99c50144c9197f84
deltaKshatriya <kunalM@seas0446.wlan.seas.upenn.edu>
2016-10-26 19:41:33 -0400

    Initial analog input reading code

---
+++ readAss.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+	//echo cape-bone-iio > /sys/devices/bone_capemgr.*/slots
+	// printf("Starting\n");
+	FILE *fp;
+	//= fopen("/sys/devices/bone_capemgr.*/slots", "rw");
+	// printf("Opened\n");
+	// fputs("cape-bone-iio", fp);
+	// printf("Written\n");
+	// fclose(fp);
+	// printf("Closed\n");
+	printf("Opened again\n");
+
+	char buff[256];
+
+
+
+
+	while (1 == 1) {
+		fp = fopen("/sys/devices/ocp.3/helper.15/AIN0", "rw");
+		if (fp == 0) {
+			printf("Ya done goofed...\n");
+			return;
+		}
+		fgets(buff, 256, fp);
+		printf("The voltage: %s\n", buff);
+		fclose(fp);
+	}
+}
+
+int scaleShit() {
+	
+}
