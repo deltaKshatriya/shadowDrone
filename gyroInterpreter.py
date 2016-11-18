e9e866e980afd4b1187778f8091912f1a379a9c1
deltaKshatriya <kunalM@seas01388.wlan.wireless-pennnet.upenn.edu>
2016-11-18 16:36:20 -0500

    Adding in the gyroscope python script

---
+++ gyroInterpreter.py
@@ -0,0 +1,13 @@
+#TOP SECRET!
+#EYES ONLY!
+#DAS IST FÜR EIN ÜBERGEHEIMNISSES PROJECT
+
+
+
+import logging
+import sys
+import time
+
+from Adafruit_BNO055 import BNO055
+
+bno = BNO055.BNO055(rst='P9_12')
