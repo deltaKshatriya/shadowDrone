7ef348125cf5a12c6956169617676bffcf235568
deltaKshatriya <kunalM@MacBook-Pro-8.local>
2016-11-25 16:17:54 -0500

    Linked list structure

--- c/utilities.h
+++ c/utilities.h
@@ -1,2 +1,13 @@
-\double coerce(double val, double min, double max);
+double coerce(double val, double min, double max);
 
+
+
+typedef struct node {
+	void *val;
+	node *next;
+} node;
+
+typedef struct linkedList {
+	node *head;
+	node *tail;
+} linkedList;
