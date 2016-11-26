00181a551005a95af305b6516976d9325cda053f
deltaKshatriya <kunalM@MacBook-Pro-8.local>
2016-11-26 10:08:27 -0500

    Python gesture orientation class

--- python/gyroGesture.py
+++ python/gyroGesture.py
@@ -1,2 +1,24 @@
+import logging
+import sys
+import time
+
+from Adafruit_BNO055 import BNO055
+
 class gyroGesture.py:
-	
+	heading = 0.0
+	roll = 0.0
+	pitch = 0.0
+	sys = 0.0
+	gyro = 0.0
+	accel = 0.0
+	mag = 0.0
+	bno = BNO055.BNO055(rst='P9_12')
+
+	def initialize:
+		bno = BNO055.BNO055(rst='P9_12')
+
+	def getOrientation: 
+		# Read the Euler angles for heading, roll, pitch (all in degrees).
+		heading, roll, pitch = bno.read_euler()
+		# Read the calibration status, 0=uncalibrated and 3=fully calibrated.
+    	sys, gyro, accel, mag = bno.get_calibration_status()
