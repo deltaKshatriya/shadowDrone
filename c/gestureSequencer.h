78b3bf61d348d8c8d1efc3c670f7df437ff1c8e4
deltaKshatriya <kunalM@MacBook-Pro-8.local>
2016-11-26 09:25:12 -0500

    Python code

---
+++ c/gestureSequencer.h
@@ -0,0 +1,3 @@
+#include "utilities.h"
+
+//we store gesture commands as sequences of gestures
--- python/gyroInterpreter.py
+++ python/gyroInterpreter.py
@@ -22,38 +22,48 @@ if status == 0x01:
 
 
 # Print BNO055 software revision and other diagnostic data.
-sw, bl, accel, mag, gyro = bno.get_revision()
-print('Software version:   {0}'.format(sw))
-print('Bootloader version: {0}'.format(bl))
-print('Accelerometer ID:   0x{0:02X}'.format(accel))
-print('Magnetometer ID:    0x{0:02X}'.format(mag))
-print('Gyroscope ID:       0x{0:02X}\n'.format(gyro))
+# sw, bl, accel, mag, gyro = bno.get_revision()
+# print('Software version:   {0}'.format(sw))
+# print('Bootloader version: {0}'.format(bl))
+# print('Accelerometer ID:   0x{0:02X}'.format(accel))
+# print('Magnetometer ID:    0x{0:02X}'.format(mag))
+# print('Gyroscope ID:       0x{0:02X}\n'.format(gyro))
 
 
-print('Reading BNO055 data, press Ctrl-C to quit...')
-while True:
+# print('Reading BNO055 data, press Ctrl-C to quit...')
+# while True:
+#     # Read the Euler angles for heading, roll, pitch (all in degrees).
+#     heading, roll, pitch = bno.read_euler()
+#     # Read the calibration status, 0=uncalibrated and 3=fully calibrated.
+#     sys, gyro, accel, mag = bno.get_calibration_status()
+#     # Print everything out.
+#     print('Heading={0:0.2F} Roll={1:0.2F} Pitch={2:0.2F}\tSys_cal={3} Gyro_cal={4} Accel_cal={5} Mag_cal={6}'.format(
+#           heading, roll, pitch, sys, gyro, accel, mag))
+#     # Other values you can optionally read:
+#     # Orientation as a quaternion:
+#     #x,y,z,w = bno.read_quaterion()
+#     # Sensor temperature in degrees Celsius:
+#     #temp_c = bno.read_temp()
+#     # Magnetometer data (in micro-Teslas):
+#     #x,y,z = bno.read_magnetometer()
+#     # Gyroscope data (in degrees per second):
+#     #x,y,z = bno.read_gyroscope()
+#     # Accelerometer data (in meters per second squared):
+#     #x,y,z = bno.read_accelerometer()
+#     # Linear acceleration data (i.e. acceleration from movement, not gravity--
+#     # returned in meters per second squared):
+#     #x,y,z = bno.read_linear_acceleration()
+#     # Gravity acceleration data (i.e. acceleration just from gravity--returned
+#     # in meters per second squared):
+#     #x,y,z = bno.read_gravity()
+#     # Sleep for a second until the next reading.
+
+def initializeBNO():
+    bno = BNO055.BNO055(rst='P9_12')
+
+def getOrientation():
     # Read the Euler angles for heading, roll, pitch (all in degrees).
     heading, roll, pitch = bno.read_euler()
     # Read the calibration status, 0=uncalibrated and 3=fully calibrated.
     sys, gyro, accel, mag = bno.get_calibration_status()
-    # Print everything out.
-    print('Heading={0:0.2F} Roll={1:0.2F} Pitch={2:0.2F}\tSys_cal={3} Gyro_cal={4} Accel_cal={5} Mag_cal={6}'.format(
-          heading, roll, pitch, sys, gyro, accel, mag))
-    # Other values you can optionally read:
-    # Orientation as a quaternion:
-    #x,y,z,w = bno.read_quaterion()
-    # Sensor temperature in degrees Celsius:
-    #temp_c = bno.read_temp()
-    # Magnetometer data (in micro-Teslas):
-    #x,y,z = bno.read_magnetometer()
-    # Gyroscope data (in degrees per second):
-    #x,y,z = bno.read_gyroscope()
-    # Accelerometer data (in meters per second squared):
-    #x,y,z = bno.read_accelerometer()
-    # Linear acceleration data (i.e. acceleration from movement, not gravity--
-    # returned in meters per second squared):
-    #x,y,z = bno.read_linear_acceleration()
-    # Gravity acceleration data (i.e. acceleration just from gravity--returned
-    # in meters per second squared):
-    #x,y,z = bno.read_gravity()
-    # Sleep for a second until the next reading.
+    return [heading, roll, pitch, sys, gyro, accel, mag]
